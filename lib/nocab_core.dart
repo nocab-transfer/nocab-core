6e3fe13049591fa562e32b7262c583a5b173d78e
Berke Emin Kabagöz <berkekbgz@gmail.com>
2023-01-11 17:27:19 +0300

    Feat: Export logger with core

--- lib/nocab_core.dart
+++ lib/nocab_core.dart
@@ -19,3 +19,5 @@ export 'src/transfer/report_models/base_report.dart';
 export 'src/transfer/transfer.dart';
 export 'src/transfer/sender.dart';
 export 'src/transfer/receiver.dart';
+
+export 'package:nocab_logger/nocab_logger.dart';
